b23df0fae55d2d0d6a8248f252658af9fcc8cc49
Aayushi19271 <60641115+Aayushi19271@users.noreply.github.com>
2020-03-28 16:09:44 +0530

    Add files via upload

---
+++ spring-security-bootcamp-master/src/test/java/com/springbootcamp/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,11 @@
+package com.springbootcamp.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+
+
+}
